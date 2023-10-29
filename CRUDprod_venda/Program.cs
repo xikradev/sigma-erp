24617a8098e45ba07277d5fc9688f211af1293fb
Matias Monteiro <matiasmonteiro15@gmail.com>
2023-10-29 15:20:31 -0300

    Adicionando algoritmo de execução do container no metodo Main

--- CRUDprod_venda/Program.cs
+++ CRUDprod_venda/Program.cs
@@ -1,4 +1,6 @@
-﻿using ErpSigmaVenda.navegacao;
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using ErpSigmaVenda.navegacao;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,11 +16,51 @@ namespace ErpSigmaVenda
         /// Ponto de entrada principal para o aplicativo.
         /// </summary>
         [STAThread]
-        static void Main()
+        static async Task Main()
         {
+            using (var client = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine")).CreateClient())
+            {
+                try
+                {
+                    
+                    var containerName = "sqlserver-container"; 
+                    var startParameters = new ContainerStartParameters();
+
+                    await client.Containers.StartContainerAsync(containerName, startParameters);
+                    Console.WriteLine($"Contêiner {containerName} iniciado com sucesso.");
+                }
+                catch (Docker.DotNet.DockerApiException ex)
+                {
+                    Console.WriteLine($"Erro ao iniciar o contêiner SQL Server: {ex.Message}");
+                }
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmTelaPrincipal());
+
+            using (var client = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine")).CreateClient())
+            {
+                try
+                {
+                    
+                    var containerName = "sqlserver-container";
+                    var stopParameters = new ContainerStopParameters
+                    {
+                        WaitBeforeKillSeconds = 5
+                    };
+
+                    await client.Containers.StopContainerAsync(containerName, stopParameters);
+                    Console.WriteLine($"Contêiner {containerName} parado com sucesso.");
+                }
+                catch (Docker.DotNet.DockerApiException ex)
+                {
+                    Console.WriteLine($"Erro ao parar o contêiner SQL Server: {ex.Message}");
+                }
+            }
+
         }
+
+
     }
 }
